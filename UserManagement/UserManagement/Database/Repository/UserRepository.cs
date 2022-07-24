d650fa10f7ffedea9838f190bbfb76bb5981a9b3
nqasanova <106176658+nqasanova@users.noreply.github.com>
2022-07-24 20:16:43 +0400

    Update UserRepository.cs

--- UserManagement/UserManagement/Database/Repository/UserRepository.cs
+++ UserManagement/UserManagement/Database/Repository/UserRepository.cs
@@ -1,15 +1,17 @@
-﻿using AuthenticationWithClie.Database.Models;
+using AuthenticationWithClie.Database.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagement.Database.Models;
 
 namespace AuthenticationWithClie.Database.Repository
 {
     class UserRepository
     {
         private static int _idCounter;
+        private static List<Report> reports;
 
         public static int IdCounter
         {
@@ -24,7 +26,12 @@ namespace AuthenticationWithClie.Database.Repository
         private static List<User> Users { get; set; } = new List<User>()
         {
             new Admin("Mahmood", "Garibov", "qaribovmahmud@gmail.com", "123321"),
-            new User("Eshqin", "Mahmudov", "eshqin@gmail.com", "123321"),
+            new User("Natavan", "Hasanova", "natavanhasanova@gmail.com", "123321"),
+        };
+
+        private static List<Report> Reports { get; set; } = new List<Report>()
+        {
+            new Report("natavanhasanova@gmail.com", "mahmoodgaribovgmail.com", "Text")
         };
 
         public static User AddUser(string firstName, string lastName, string email, string password)
@@ -34,7 +41,7 @@ namespace AuthenticationWithClie.Database.Repository
             return user;
         }
 
-        public static User AddUser(string firstName, string lastName, string email, string password, int id)
+        public static User AddAdmin(string firstName, string lastName, string email, string password, int id)
         {
             User user = new User(firstName, lastName, email, password, id);
             Users.Add(user);
@@ -47,20 +54,10 @@ namespace AuthenticationWithClie.Database.Repository
             return user;
         }
 
-        public static User AddUser(Admin user)
+        public static User AddAdmin(Admin admin)
         {
-            Users.Add(user);
-            return user;
-        }
-
-        public static List<User> GetAll()
-        {
-            return Users;
-        }
-
-        public static int GetUserCount()
-        {
-            return Users.Count;
+            Users.Add(admin);
+            return admin;
         }
 
         public static bool IsUserExistsByEmail(string email)
@@ -115,9 +112,78 @@ namespace AuthenticationWithClie.Database.Repository
             return null;
         }
 
-        public static void Delete(User user)
+        public static Admin GetAdminByEmail(string email)
+        {
+            foreach (Admin admin in Users)
+            {
+                if (admin.Email == email)
+                {
+                    return admin;
+                }
+            }
+
+            return null;
+        }
+        public static void Remove(string email)
+        {
+            foreach (User user in Users)
+            {
+                if(user.Email == email)
+                { }
+                Users.Remove(user);
+            }
+        }
+
+        public static Report AddReport(string reporter, string receiver, string, string text)
+        {
+            List<Report> reports = new List<Report>();
+            Report report = new Report(reporter, receiver, text);
+            reports.Add(report);
+            return report;
+        }
+
+        public static void DeleteUser(User user)
         {
             Users.Remove(user);
+        }
+
+        public static void DeleteAdmin(Admin admin)
+        {
+            Users.Remove(admin);
+        }
+
+        public static List<User> GetAll()
+        {
+            return Users;
+        }
+
+        public static int GetUserCount()
+        {
+            return Users.Count;
+        }
+
+        public static List<Report> GetReports()
+        {
+            return reports;
+        }
+
+        public static User UpdateUser(string email, User user)
+        {
+            User enteredUser = UserRepository.GetUserByEmail(email);
+
+            enteredUser.FirstName = user.FirstName;
+            enteredUser.LastName = user.LastName;
+
+            return enteredUser;
+        }
+
+        public static User UpdateAdmin(string email, Admin admin)
+        {
+            User enteredAdmin = UserRepository.GetUserByEmail(email);
+            enteredAdmin.FirstName = admin.FirstName;
+            enteredAdmin.LastName = admin.LastName;
+
+            return enteredAdmin;
         }
     }
 }
